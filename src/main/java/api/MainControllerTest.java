91267c285669caf512a4ce231ffa5b2068c17aa8
guillaumeBonnet <bonnet.guillaume.93@gmail.com>
2019-06-01 19:10:59 +0200

    [ERROR] /tmp/build_b84cda6aee6bb11a4bc2e850e002d224/src/main/java/api/MainControllerTest.java:[3,24] package org.junit.runner does not exist

--- src/main/java/api/MainControllerTest.java
+++ src/main/java/api/MainControllerTest.java
@@ -1,15 +1,15 @@
 package api;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
+// import org.junit.runner.RunWith;
+// import org.springframework.test.context.ContextConfiguration;
+// import org.springframework.test.context.TestContext;
+// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+// import org.springframework.test.context.web.WebAppConfiguration;
+// import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestContext.class, WebApplicationContext.class})
-@WebAppConfiguration
-public class MainControllerTest {
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(classes = {TestContext.class, WebApplicationContext.class})
+// @WebAppConfiguration
+// public class MainControllerTest {
 
-}
+// }
